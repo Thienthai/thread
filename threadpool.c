099f295e342e6f100924b1b0f7188a054b9ebd64
Thienthai <surkessone@gmail.com>
2017-06-14 23:56:37 +0700

    commit me

--- threadpool.c
+++ threadpool.c
@@ -13,9 +13,9 @@
 
 // use this struc to record the work perform by thread
 typedef struct record_t {
-	void (*routine) (void*);
-	void * arg;
-	struct record* next;
+	void (*routine) (void*); // assign the pre void function
+	void * arg; // assign for thread argument
+	struct record_t* next; // the next thread assign
 } record_t;
 
 // _threadpool is the internal threadpool structure that is
@@ -24,7 +24,7 @@ typedef struct _threadpool_st {
     int threadsize;
     pthread_mutex_t lock_x;
     pthread_cond_t lock_x_empty;   // use to check whether is running
-    pthread_cond_t lock_x_nonempty; // check wether is it not running
+    pthread_cond_t lock_x_nonempty; // check weather is it not running
     pthread_t *thrd;
     int quesz; // record number of queue
     record_t* front;
@@ -35,12 +35,54 @@ typedef struct _threadpool_st {
 
 
 void *worker_thread(void *args) {
+    _threadpool * pool = (_threadpool *) args;
+    record_t* seq; // sequence record 
     while (1) {
         // wait for a signal
         // l
         // mark itself as busy
         // run a given function
         //
+        pthread_mutex_lock(&(pool->lock_x)); //lock it
+        while(pool->quesz == 0){
+            if(pool->close){
+                pthread_mutex_unlock(&(pool->lock_x));
+                pthread_exit(NULL);
+            }
+            // create each thread and wait for the dispatch
+            pthread_mutex_unlock(&(pool->lock_x));
+            pthread_cond_wait(&(pool->lock_x_nonempty),&(pool
+            ->lock_x)); 
+            
+            if(pool->close){
+                // after finish dispatch unlock other thread
+                // to work
+                pthread_mutex_unlock(&(pool->lock_x));
+                pthread_exit(NULL);
+            }
+        }
+        
+        //count down the sequence after finish job 
+        seq = pool->front;
+        pool->quesz--;
+        
+        if(pool->quesz == 0){
+            // if no sequence before just set the front and
+            // back to null
+            pool->front = NULL;
+            pool->back = NULL;
+        }
+        else{
+            // if have sequence already not empty
+            // assign the next routine to it
+            pool->front = seq->next;
+        }
+        
+        // allow other thread to come in working thread
+        // and free all the current work
+        pthread_mutex_unlock(&(pool->lock_x));
+        (seq->routine) (seq->arg);
+        free(seq);
     }
 }
 
@@ -64,6 +106,7 @@ threadpool create_threadpool(int num_threads_in_pool) {
     fprintf(stderr, "Out of memory creating a new threadpool!\n");
     return NULL;
   }
+  // initiallize
   pool -> threadsize = num_threads_in_pool;
   pool -> quesz = 0;
   pool -> front = NULL;
@@ -74,11 +117,6 @@ threadpool create_threadpool(int num_threads_in_pool) {
   if(pthread_mutex_init(&pool -> lock_x,NULL)){ // initiallize mutex
      fprintf(stderr, "mut error!\n"); // if error print it out
 	   return NULL;
-  }
-
-  if(pthread_cond_init(&(pool -> lock_x_empty),NULL)){ // inintiallize conditional var
-    fprintf(stderr,"conditional var error!"); // if error print it out
-    return NULL;
   }
 
   if(pthread_cond_init(&(pool -> lock_x_nonempty),NULL)){ // inintiallize conditional var
@@ -102,12 +140,42 @@ void dispatch(threadpool from_me, dispatch_fn dispatch_to_here,
   _threadpool *pool = (_threadpool *) from_me;
 
   // add your code here to dispatch a thread
-
-
+  // make the sequence process of thread
+  record_t * seq;
+  seq = (record_t*) malloc(sizeof(record_t));
+  if(seq == NULL){
+      fprintf(stderr,"mem error");// in case that we run out of memory
+      return;
+  }
+  // assign all thread work
+  seq->routine = dispatch_to_here;
+  seq->arg = arg;
+  seq->next = NULL;
+  
+  pthread_mutex_lock(&(pool->lock_x)); // start the mutex
+  
+  if(pool->quesz == 0) // starting the thread go into this loop
+  {
+      //set the sequence to be the starting point of seq
+      pool->front = seq; 
+      pool->back = seq;
+      pthread_cond_signal(&(pool->lock_x_nonempty)); // set thread work to be none empty     
+  }else{
+      // assign the next thread and the current working thread
+      pool->back->next = seq;
+      pool->back = seq;
+  }
+  // increase more sequence and allow other thread to enter to this area
+  pool->quesz++;
+  pthread_mutex_unlock(&(pool->lock_x));
 }
 
 void destroy_threadpool(threadpool destroyme) {
   _threadpool *pool = (_threadpool *) destroyme;
-
   // add your code here to kill a threadpool
+  // free thread and destroy all pthread
+  free(pool->thrd);
+  pthread_mutex_destroy(&(pool->lock_x));
+  pthread_cond_destroy(&(pool->lock_x_nonempty));
+  return;
 }
